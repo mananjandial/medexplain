04f48637fd524bb5fe6362a5a3cdf41b19e556b9
mananjandial <jandial2001@gmail.com>
2026-01-21 17:07:39 +0530

    Add root URL redirect to login

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 
 urlpatterns = [
@@ -32,10 +33,16 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-urlpatterns += [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('signup/', include('reports.urls')),
-    path('', include('users.urls')),
+# urlpatterns += [
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+#     path('signup/', include('reports.urls')),
+#     path('', include('users.urls')),
 
+# ]
+urlpatterns = [
+    path('', lambda request: redirect('/login/')),
+    path('admin/', admin.site.urls),
+    path('', include('reports.urls')),
+    path('', include('users.urls')),
 ]
