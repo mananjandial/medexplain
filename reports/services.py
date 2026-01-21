ea3ba2398c49df12b8ee7beffcb1afa26dac2b6a
mananjandial <jandial2001@gmail.com>
2026-01-21 12:46:51 +0530

    Add build script for OCR (Tesseract)

--- reports/services.py
+++ reports/services.py
@@ -1,15 +1,42 @@
+# from PyPDF2 import PdfReader
+
+# def extract_text_from_file(file_path):
+#     text = ""
+
+#     if file_path.endswith('.pdf'):
+#         reader = PdfReader(file_path)
+#         for page in reader.pages:
+#             text += page.extract_text() or ""
+
+#     elif file_path.endswith('.txt'):
+#         with open(file_path, 'r', encoding='utf-8') as f:
+#             text = f.read()
+
+#     return text.strip()
+# old code till herer ####################################
 from PyPDF2 import PdfReader
+from PIL import Image
+import pytesseract
+import os
 
 def extract_text_from_file(file_path):
-    text = ""
+    extracted_text = ""
+    ext = os.path.splitext(file_path)[1].lower()
 
-    if file_path.endswith('.pdf'):
+    # PDF
+    if ext == ".pdf":
         reader = PdfReader(file_path)
         for page in reader.pages:
-            text += page.extract_text() or ""
+            extracted_text += page.extract_text() or ""
 
-    elif file_path.endswith('.txt'):
-        with open(file_path, 'r', encoding='utf-8') as f:
-            text = f.read()
+    # TEXT
+    elif ext == ".txt":
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+            extracted_text = f.read()
 
-    return text.strip()
+    # IMAGE (OCR)
+    elif ext in [".png", ".jpg", ".jpeg"]:
+        image = Image.open(file_path)
+        extracted_text = pytesseract.image_to_string(image)
+
+    return extracted_text.strip()
